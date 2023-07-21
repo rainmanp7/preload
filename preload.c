b9fd58cef6a24b5f9c0d4a094d5d8f4641388918
Area15 R&D <muslimsoap@gmail.com>
2023-07-21 13:04:52 +0800

    Update preload.c
    ● I removed the GPOINTER_TO_INT() macro from the sig_handler_sync()
    function.
    ● I declared the main_loop variable as a local variable

--- preload.c
+++ preload.c
@@ -1,26 +1,15 @@
-// the elements corrected are already referenced in. other files 
 #include "common.h"
-
 #include "preload.h"
-
 #include "log.h"
-
 #include "cmdline.h"
-
 #include "conf.h"
-
 #include "state.h"
-
 #include <signal.h>
-
 #include <grp.h>
 
 /* variables */
-
 const char *conffile = DEFAULT_CONFFILE;
-
 const char *statefile = DEFAULT_STATEFILE;
-
 const char *logfile = DEFAULT_LOGFILE;
 
 int nicelevel = DEFAULT_NICELEVEL;
@@ -38,11 +27,8 @@ main (int argc, char **argv)
 /* initialize */
 
 preload_cmdline_parse (&argc, &argv);
-
 preload_log_init (logfile);
-
 preload_conf_load (conffile, TRUE);
-
 set_sig_handlers ();
 
 if (!foreground)
@@ -52,17 +38,13 @@ daemonize ();
 if (0 > nice (nicelevel))
 
 g_warning ("%s", strerror (errno));
-
 g_debug ("starting up");
-
 preload_state_load (statefile);
 
 /* main loop */
 
 GMainLoop *main_loop = g_main_loop_new (NULL, FALSE);
-
 preload_state_run (statefile);
-
 g_main_loop_run (main_loop);
 
 /* clean up */
@@ -70,11 +52,36 @@ g_main_loop_run (main_loop);
 preload_state_save (statefile);
 
 if (preload_is_debugging ())
-
 preload_state_free ();
-
 g_debug ("exiting");
-
 return EXIT_SUCCESS;
+}
+static gboolean
 
+sig_handler_sync (gpointer data)
+{
+
+int signal_number = (int) data;
+switch (signal_number) {
+case SIGHUP:
+preload_conf_load (conffile, FALSE);
+preload_log_reopen (logfile);
+break;
+
+case SIGUSR1:
+preload_state_dump_log ();
+break;
+
+case SIGUSR2:
+preload_state_save (statefile);
+
+break;
+
+default: /* everything else is an exit request */
+g_message ("exit requested");
+g_main_loop_quit (main_loop);
+break;
+}
+
+return FALSE;
 }
